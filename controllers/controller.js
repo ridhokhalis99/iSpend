88bac3890c4b516e0c8732145c861b97f80a1549
ridhokhalis99 <ridhokholis9a@gmail.com>
2022-05-26 00:09:24 +0700

    addRoutes

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,6 +1,42 @@
 class Controller {
+    static login(req, res){
+
+    }
+
+    static signUp(req, res){
+        
+    }
+
     static index(req, res){
-        res.send('test')
+
+    }
+
+    static addTransactionForm(req, res){
+        
+    }
+
+    static addTransaction(req, res){
+
+    }
+
+    static showProfile(req, res){
+
+    }
+
+    static editProfileForm(req, res){
+
+    }
+
+    static editProfile(req, res){
+
+    }
+
+    static userList(req, res){
+
+    }
+
+    static deleteUser(req, res){
+        
     }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,15 @@
 const router = require('express').Router()
 const Controller = require('../controllers/controller')
 
+router.get('/login', Controller.login)
+router.get('/signUp', Controller.signUp)
 router.get('/', Controller.index)
+router.get('/addTransaction', Controller.addTransactionForm)
+router.post('/addTransaction', Controller.addTransaction)
+router.get('/profile', Controller.showProfile)
+router.get('/profile/edit', Controller.editProfileForm)
+router.post('/profile/edit', Controller.editProfile)
+router.get('/users', Controller.userList)
+router.get('/users/:UserId/delete', Controller.deleteUser)
 
 module.exports = router
