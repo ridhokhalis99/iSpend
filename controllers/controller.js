2c7cf2560b30a1c761788847031b2a9d29fa11b0
Raymond Wangsaatmadja <raykur9@gmail.com>
2022-05-26 09:50:21 +0700

    login signup page, app page

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,10 +1,55 @@
+const { User, Profile, Transaction } = require('../models')
+
+
 class Controller {
     static login(req, res){
-
+        res.render('login')
     }
 
     static signUp(req, res){
-        
+        res.render('signup')
+    }
+
+
+    static iSpend(req,res){
+        const {id} = req.params
+        // res.send(id)
+        const {filter} = req.query
+        // console.log(filter)
+        let option = {
+            include: [Profile, {
+                model: Transaction
+                
+            }]
+        }
+
+        if (filter) {
+            option = {
+                include: [Profile, {
+                    model: Transaction,
+                    where: {
+                        category: filter
+                    }
+                }]
+            }
+        }
+
+        let dataBar = {}
+
+        User.findByPk(id,{
+            include: [Profile, Transaction]
+        })
+        .then((dataForBar)=>{
+            dataBar = dataForBar
+            return User.findByPk(id,option)
+        })
+        .then((dataUser)=>{
+            res.render('iSpend',{dataUser,dataBar})
+        })
+        .catch((err)=>{
+            res.send(err)
+        })
+
     }
 
     static index(req, res){
--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,21 @@ const router = require('express').Router()
 const Controller = require('../controllers/controller')
 
 router.get('/login', Controller.login)
+// router.post('/login', Controller.login)
 router.get('/signUp', Controller.signUp)
-router.get('/', Controller.index)
-router.get('/addTransaction', Controller.addTransactionForm)
-router.post('/addTransaction', Controller.addTransaction)
-router.get('/profile', Controller.showProfile)
-router.get('/profile/edit', Controller.editProfileForm)
-router.post('/profile/edit', Controller.editProfile)
-router.get('/users', Controller.userList)
-router.get('/users/:UserId/delete', Controller.deleteUser)
+// router.post('/signUp', Controller.signUp)
+
+
+router.get('/users/:id', Controller.iSpend) // bikin sementara soalnya mau pake :id biar nampilin data yang sesuai id user
+
+
+// router.get('/', Controller.index)
+// router.get('/addTransaction', Controller.addTransactionForm)
+// router.post('/addTransaction', Controller.addTransaction)
+// router.get('/profile', Controller.showProfile)
+// router.get('/profile/edit', Controller.editProfileForm)
+// router.post('/profile/edit', Controller.editProfile)
+// router.get('/users', Controller.userList)
+// router.get('/users/:UserId/delete', Controller.deleteUser)
 
 module.exports = router
